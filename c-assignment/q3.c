61486ff59963563549d8e111c0fbeb7a68567156
Sai-Janjirala <saijanjirala5411@gmail.com>
2025-09-15 10:27:44 +0530

    c assignments added

---
+++ c-assignment/q3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    for (int i = 65; i <= 65 + n; i++)
+    {
+        printf("%c\n", i);
+    }
+    return 0;
+}
---
+++ c-assignment/q6.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 65; j < 65 + n; j++)
+        {
+            printf("%c", j);
+        }
+        printf("\n");
+    }
+    return 0;
+}
